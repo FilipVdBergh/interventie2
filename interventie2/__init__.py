4aa49a17d11fa873b8aecc8a9886caacfea16d6e
FilipVdBergh <f.s.vandenbergh@gmail.com>
2024-03-05 18:49:17 +0100

    Keeping more database connections alive to prevent timeouts on Azure.

--- interventie2/__init__.py
+++ interventie2/__init__.py
@@ -11,6 +11,13 @@ app = Flask(__name__)
 app.config.from_object(config.DevelopmentConfig)
 app.config.from_envvar('CONFIG_FILE', silent=True)
 
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
+    'pool_size': 10,
+    'pool_recycle': 60,
+    'pool_pre_ping': True
+}
+
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = "main.login"
