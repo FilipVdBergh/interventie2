c4f4b4e25df881bef7612bd4dc4aff51e32762bc
FilipVdBergh <f.s.vandenbergh@gmail.com>
2024-03-06 18:35:41 +0100

    Groundwork for interface changes

---
+++ interventie2/static/scripts/switch_tab.js
@@ -0,0 +1,21 @@
+// https://www.w3schools.com/howto/howto_js_tabs.asp
+function switch_tab(evt, tabName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+  
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    // Get all elements with class="tablinks" and remove the class "active_tab"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active_tab", "");
+    }
+  
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active_tab";
+  }
