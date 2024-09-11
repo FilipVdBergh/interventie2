8d9251e0fd2e7754eda3cb87b98dedbc70a1d020
FilipVdBergh <f.s.vandenbergh@gmail.com>
2024-09-11 19:00:35 +0200

    Fixed ENV variables

--- interventie2/config.py
+++ interventie2/config.py
@@ -9,6 +9,7 @@ class BaseConfig(object):
 	APP_NAME = environ.get('APP_NAME', 'interventie2')
 	ALLOW_CATALOG_VIEW = environ.get('ALLOW_CATALOG_VIEW', False)
 	ALLOW_DB_INIT = environ.get('ALLOW_DB_INIT', False)
+	ALLOW_SELF_REGISTER = environ.get('ALLOW_SELF_REGISTER', False)
 	MAINTAINER = environ.get('MAINTAINER', 'interventieteam')
 	MAINTAINER_EMAIL = environ.get('MAINTAINER_EMAIL', 'interventie@afm.nl')
 	SQLALCHEMY_DATABASE_URI = environ.get('SQLALCHEMY_DATABASE_URI')
