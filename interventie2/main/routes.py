a7bab030b63ed9d1082901fee591682911be086c
FilipVdBergh <f.s.vandenbergh@gmail.com>
2025-03-10 18:37:53 +0100

    Fixed #51

--- interventie2/main/routes.py
+++ interventie2/main/routes.py
@@ -156,6 +156,7 @@ def new_worksession_from_base(question_set_id, base_session_id=None):
 		worksession_base = Worksession.query.get(base_session_id)
 		worksession.name = worksession_base.name
 		worksession.description = worksession_base.description
+		worksession.effect = worksession_base.effect
 		worksession.project_number = worksession_base.project_number
 		worksession.link_to_page = worksession_base.link_to_page
 	worksession.date = datetime.today()
@@ -375,59 +376,6 @@ def delete_worksession(worksession_id):
 	db.session.delete(worksession)
 	db.session.commit()
 	return redirect(url_for('main.index'))
-
-
-@main.route('/worksession/<int:worksession_id>/present', methods=['GET', 'POST'])
-@main.route('/worksession/<int:worksession_id>/present/<int:question_id>', methods=['GET', 'POST'])
-@login_required
-def present(worksession_id, question_id=None):
-	worksession = Worksession.query.get(worksession_id)
-	if not current_user.role.see_all_worksessions and current_user not in Worksession.query.get(worksession_id).allowed_users:
-		return render_template('error/index.html', title='Onvoldoende rechten', message='Onvoldoende rechten om deze sessie te zien.')
-	
-	advisor = Advisor(worksession=worksession, instruments=Instrument.query.all())
-	if question_id is None:
-		question = Question.query.filter_by(question_set=worksession.question_set).order_by(Question.order).first()
-	else:	
-		question = Question.query.get(question_id)
-	answer = Answer.query.filter_by(worksession=worksession, question=question).first()
-	
-	if request.method == "POST":
-		for answer in Answer.query.filter_by(worksession=worksession):
-			# Delete all answers to replace them below.
-			for selection in answer.selection:
-				db.session.delete(selection)
-			db.session.delete(answer)
-
-		motivations = {}
-		selected_options = []
-		weights = {}
-		for item, value in request.form.items():
-			# De name-attribute van de textarea bevat het soort vraag (motivation, option), een :, en dan het vraagnummer of het optienummer.
-			if 'motivation' in item:
-				_, question_id = item.split(':', 1)
-				motivations[int(question_id)] = value
-			if 'option' in item:
-				selected_options.append(int(value))
-			if 'weight' in item:
-				_, question_id = item.split(':', 1)
-				weights[int(question_id)] = value
-
-		for question in worksession.question_set.questions:
-		# Alleen de vragen in de huidige question set
-			if not question.is_category:
-				new_answer = Answer(worksession=worksession, question=question, motivation=motivations.get(question.id), weight=weights.get(question.id))
-				for option in question.options:
-					if option.id in selected_options: 
-						# De vraag zit in de huidige question set en moet aangevinkt worden.
-						new_answer.selection.append(Selection(option=option))
-				db.session.add(new_answer)
-		db.session.commit()
-		return redirect(url_for('main.present', worksession_id=worksession.id))
-	return render_template('main/present.html', 
-						worksession=worksession,
-						current_question=question,
-						advisor=advisor)
 
 
 @main.route('/worksession/<int:worksession_id>/simultaneous', methods=['GET', 'POST'])
